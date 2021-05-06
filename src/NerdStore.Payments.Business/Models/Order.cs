5a3fb5cc1ae85a894cfcbc0edb411e9ff14b3ef5
Washington <washingtonalmeidaco@gmail.com>
2021-05-06 02:11:13 -0300

    Added the classes of the order and product model of the payment context.

---
+++ src/NerdStore.Payments.Business/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Payments.Business.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ src/NerdStore.Payments.Business/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿
+namespace NerdStore.Payments.Business.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
